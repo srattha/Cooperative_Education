0f162e899bdf67370b5790b36486ea8a97aa48fb
srattha <srattha.s@gmail.com>
2019-01-04 14:24:59 +0700

    .

--- resources/views/download/download.blade.php
+++ resources/views/download/download.blade.php
@@ -18,58 +18,54 @@
 </style>
 @endsection
 @section('content')
-<div class="container-fluid panel panel-default" style="margin-top: -20px;">
-   <h1 class="head-page-title" style="color:  #3F51B5;">ดาวน์โหลด</h1>
-   <hr>
-   <p>หน้าสำหรับการดาวน์โหลดโปรแกรม เอกสาร แบบฟอร์มสหกิจศึกษา </p>
-   <table class="table table-bordered" style="background-color: #fff;">
-      <tbody>
-         <tr>
-            <td class="well">
-               <p 18.5714px line-height: style="color: rgb(0, 105, 214);">เอกสารสหกิจศึกษาต่างๆ </p>
-               <a href=""><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>เอกสารหมายเลข 1 </a>
-               <br>
-               <a href=""><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>เอกสารหมายเลข 2 </a>
-               <br>
-               <a href=""><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>เอกสารหมายเลข 3 </a>
-               <br>
-               <a href=""><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>เอกสารหมายเลข 4 </a>
-               <br>
-               <a href=""><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>เอกสารหมายเลข 5 </a>
-               <br>
-               <a href=""><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>เอกสารหมายเลข 6 </a>
-               <br>
-               <a href=""><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>เอกสารหมายเลข 7 </a>
-               <br>
-               <a href=""><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>เอกสารหมายเลข 8 </a>
-               <br>
-               <a href=""><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>เอกสารหมายเลข 9 </a>
-               <br>
-               <a href=""><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>เอกสารหมายเลข 10 </a>
-               <br>
-               <a href=""><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>เอกสารหมายเลข 11 </a>
-               <br>
-               <a href=""><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>เอกสารหมายเลข 12 </a>
-            </td>
-         </tr>
-      </tbody>
-   </table>
-   <br>
-</div>
-<br>
-<footer class="footer">
-      <span class="footer-divider" style="height: 5px;
-         display: block;
-         background: #3F51B5;"></span>
-      <div class="col-md-12" style="margin-top: 50px"></div>
-      <div class="container">
-         <div class="">
-            <p>Copyright <i class="fa fa-copyright"> 2018-Cooperative Education</i></p>
-            Deverloped By Patiphan Uthang And Yuwa Saewang
-         </div>
+<div class="container-fluid panel panel-default" style="margin-top: -10px;">
+   <div class="row">
+      <div class="col-md-12">
+         <h1 class="head-page-title" style="color: #3f51b5;">ดาวน์โหลด</h1>
       </div>
-      <div class="row" style="margin-top: 50px"></div>
-   </footer>
+   </div>
+   <hr>
+   <div class="row">
+      <div class="col-md-12">
+         <p>หน้าสำหรับการดาวน์โหลดโปรแกรม เอกสารต่างๆ&nbsp;</p>
+         <p>&nbsp;</p>
+         <table class="table table-bordered">
+            <tbody>
+               <tr>
+                  <td>
+                     <p 18.5714px="" line-height:="">แบบฟอร์มสหกิจศึกษาต่างๆ<br />
+                        &nbsp; &nbsp; เอกสารหมายเลข 1&nbsp;&nbsp;<a href="" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>&nbsp;แบบแจ้งรายละเอียดการเข้าร่วมโครงการสหกิจศึกษา </a><br />
+                        &nbsp; &nbsp; เอกสารหมายเลข 2&nbsp;&nbsp;<a href="" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>&nbsp;แบบตอบกลับนักศึกษาโครงการสหกิจศึกษา</a><br />
+                        <span style="line-height: 1.42857;">&nbsp; &nbsp; เอกสารหมายเลข 3&nbsp;&nbsp;<a href="" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>&nbsp;ใบสมัครงานโครงการสหกิจศึกษา</a></span><br />
+                        <span style="line-height: 1.42857;">&nbsp;&nbsp;&nbsp; เอกสารหมายเลข 4&nbsp;&nbsp;</span><font color="#337ab7"><a href=""><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>&nbsp;แบบแจ้งรายชื่อนักศึกษาโครงการสหกิจศึกษา</a></font><br />
+                        <span style="line-height: 1.42857;">&nbsp; &nbsp; เอกสารหมายเลข 5&nbsp;&nbsp;<a href="" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>&nbsp;แแบแจ้งรายละเอียดสถานประกอบการโครงการสหกิจศึกษา</a></span><br />
+                        <span style="line-height: 1.42857;">&nbsp; &nbsp; เอกสารหมายเลข 6&nbsp;&nbsp;<a href="" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>&nbsp;แบบแจ้งรายละเอียดงาน ตำแหน่งงาน พนักงานที่ปรึกษาโครงการสหกิจศึกษา</a></span><br />
+                        <span style="line-height: 1.42857;">&nbsp; &nbsp; เอกสารหมายเลข 7&nbsp;&nbsp;<a href="" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>&nbsp;แบบแจ้งแผนปฏอบัติงานโครงการสหกิจศึกษา</a></span><br />
+                        <span style="line-height: 1.42857;">&nbsp; &nbsp; เอกสารหมายเลข 8&nbsp;&nbsp;<a href="" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>&nbsp;แบบแจ้งโครงร่างรายงานการปฏิบัติงานโครงการสหกิจศึกษา</a></span><br />
+                        <span style="line-height: 1.42857;">&nbsp; &nbsp; เอกสารหมายเลข 9&nbsp;&nbsp;<a href="" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>&nbsp;แบบแจ้งยืนยันการนิเทศงานนักศึกษาโครงการสหกิจศึกษา</a></span><br />
+                        <span style="line-height: 1.42857;">&nbsp; &nbsp; เอกสารหมายเลข 10&nbsp;&nbsp;<a href="" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>&nbsp;แบบบันทึกการนิเทศงานโครงการสหกิจศึกษา</a></span><br />
+                        <span style="line-height: 1.42857;">&nbsp; &nbsp; เอกสารหมายเลข 11&nbsp;&nbsp;<a href="" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>&nbsp;แบบประเมินผลนักศึกษาโครงการสหกิจศึกษา</a></span><br />
+                        <span style="line-height: 1.42857;">&nbsp; &nbsp; เอกสารหมายเลข 12&nbsp;&nbsp;<a href="" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true" id="pdf"></i>&nbsp;แบบประเมินรางานนักศึกษาโครงการสหกิจศึกษา</a></span><br />
+                  </td>
+               </tr>
+            </tbody>
+         </table>
+      </div>
+   </div>
+</div>
+<footer class="footer">
+   <span class="footer-divider" style="height: 5px;
+      display: block;
+      background: #3F51B5;"></span>
+   <div class="col-md-12" style="margin-top: 50px"></div>
+   <div class="container">
+      <div class="">
+         <p>Copyright <i class="fa fa-copyright"> 2018-Cooperative Education</i></p>
+         Deverloped By Patiphan Uthang And Yuwa Saewang
+      </div>
+   </div>
+   <div class="row" style="margin-top: 50px"></div>
+</footer>
 @endsection
 @section('javascript')
 @endsection
