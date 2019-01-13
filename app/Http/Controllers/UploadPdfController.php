243bb0766881519808c29c4030356f537b92fa78
srattha <srattha.s@gmail.com>
2019-01-13 22:41:15 +0700

    .

--- app/Http/Controllers/UploadPdfController.php
+++ app/Http/Controllers/UploadPdfController.php
@@ -53,9 +53,8 @@ class UploadPdfController extends Controller
 
            $file = $request->file('file');
             $extension = $file->getClientOriginalExtension(); // you can also use file name
-            $name = $fileName->getClientOriginalExtensionName();
+            //$name = $fileName->getClientOriginalExtensionName();
             $fileName = time().'.'.$extension;
-            $fileName = name().'.'.$name;
             $path = public_path().'/adminpdf';
             $uplaod = $file->move($path,$fileName); 
            $fileModel = new File;
